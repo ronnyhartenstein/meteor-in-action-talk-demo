0774e7c35fc6c18f32b737258d2b4165b78cbaa7
Ronny Hartenstein <ronny@rh-flow.de>
2017-10-22 22:38:38 +0200

    Search & filter collection

--- imports/ui/list.js
+++ imports/ui/list.js
@@ -5,6 +5,7 @@ import './list.html'
 
 Template.body.onCreated(function helloOnCreated() {
     this.counter = new ReactiveVar(0)
+    this.search = new ReactiveVar('')
 
     Meteor.subscribe('playgrounds.all')
 })
@@ -14,16 +15,37 @@ Template.body.helpers({
         return Template.instance().counter.get()
     },
     playgrounds() {
-        return Playgrounds.find({})
+        const search = Template.instance().search.get()
+        if (search !== '') {
+            return Playgrounds.find({name: {$regex: new RegExp(search, 'i')}})
+        } else {
+            return Playgrounds.find()
+        }
     },
+    search() {
+        return Template.instance().search.get()
+    }
 })
 
 Template.body.events({
     'click button'(event, instance) {
         instance.counter.set(instance.counter.get() + 1)
     },
+    'keyup input[name=search]'(event, instance) {
+        console.log("woop")
+        const input = $(event.target)
+        instance.search.set(input.val())
+    }
 })
 
+
+
+
+Template.listitem.helpers({
+    commentsCount() {
+        return this.comments.length
+    }
+})
 Template.listitem.events({
     'click .map'(event) {
         const [h, v] = this.coordinates
